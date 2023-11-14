5cd8f9403da42493486c151502651553941c7b67
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2023-11-14 11:43:13 +0200

    Vector DB and prompts chunking

--- llm/wrapper.py
+++ llm/wrapper.py
@@ -24,22 +24,33 @@ def set_qa_prompt():
     return prompt
 
 
-def build_retrieval_qa_chain(llm, prompt, vectordb):
+def build_retrieval_qa_chain(llm, prompt):
+    embeddings = HuggingFaceEmbeddings(model_name=cfg.EMBEDDINGS,
+                                       model_kwargs={'device': 'cpu'})
+    vectordb = FAISS.load_local(cfg.DB_FAISS_PATH, embeddings)
+    retriever = vectordb.as_retriever(search_kwargs={'k': cfg.VECTOR_COUNT})
+
     qa_chain = RetrievalQA.from_chain_type(llm=llm,
-                                       chain_type='stuff',
-                                       retriever=vectordb.as_retriever(search_kwargs={'k': cfg.VECTOR_COUNT}),
-                                       return_source_documents=cfg.RETURN_SOURCE_DOCUMENTS,
-                                       chain_type_kwargs={'prompt': prompt}
-                                       )
+                                           chain_type='stuff',
+                                           retriever=retriever,
+                                           return_source_documents=cfg.RETURN_SOURCE_DOCUMENTS,
+                                           chain_type_kwargs={'prompt': prompt})
     return qa_chain
 
 
 def setup_qa_chain():
+    llm = setup_llm()
+    qa_prompt = set_qa_prompt()
+    qa_chain = build_retrieval_qa_chain(llm, qa_prompt)
+
+    return qa_chain
+
+
+def query_embeddings(query):
     embeddings = HuggingFaceEmbeddings(model_name=cfg.EMBEDDINGS,
                                        model_kwargs={'device': 'cpu'})
     vectordb = FAISS.load_local(cfg.DB_FAISS_PATH, embeddings)
-    llm = setup_llm()
-    qa_prompt = set_qa_prompt()
-    qa_chain = build_retrieval_qa_chain(llm, qa_prompt, vectordb)
+    retriever = vectordb.as_retriever(search_kwargs={'k': cfg.VECTOR_COUNT})
+    semantic_search = retriever.get_relevant_documents(query)
 
-    return qa_chain
+    return semantic_search
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import timeit
 import argparse
 from llm.wrapper import setup_qa_chain
+from llm.wrapper import query_embeddings
 
 
 if __name__ == "__main__":
@@ -9,14 +10,22 @@ if __name__ == "__main__":
                         type=str,
                         default='What is the invoice number value?',
                         help='Enter the query to pass into the LLM')
+    parser.add_argument('--semantic_search',
+                        type=bool,
+                        default=False,
+                        help='Enter True if you want to run semantic search, else False')
     args = parser.parse_args()
 
     start = timeit.default_timer()
-    qa_chain = setup_qa_chain()
-    response = qa_chain({'query': args.input})
+    if args.semantic_search:
+        semantic_search = query_embeddings(args.input)
+        print(f'Semantic search: {semantic_search}')
+        print('='*50)
+    else:
+        qa_chain = setup_qa_chain()
+        response = qa_chain({'query': args.input})
+        print(f'\nAnswer: {response["result"]}')
+        print('=' * 50)
     end = timeit.default_timer()
-
-    print(f'\nAnswer: {response["result"]}')
-    print('='*50)
 
     print(f"Time to retrieve answer: {end - start}")
